2e4d98fc3e1f5e1fa74a811c1cc590d7755ff8bc
snunemo <daniel0323@snu.ac.kr>
2024-06-07 11:02:45 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -7,7 +7,28 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
+    vec.push_back(cnt+1);
+    if (cnt+1 > N) {
+	vec.pop_back();
+	return;
+    }
+    else {
+	if (vec.size() == M) {
+	    for (int a : vec) {
+	        cout << a << " ";
+	    }
+	    cout << endl;
+	    vec.pop_back();
+	    return;
+	}
+    }
+    for (int j = cnt + 1; j <= N-1; j++) {
+	    func(j);
+    }
+    vec.pop_back();
+    if (cnt < N && vec.size() == 0) {
+	    func(cnt + 1);
+    }
 
 
 
